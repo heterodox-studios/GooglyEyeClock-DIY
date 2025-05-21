dc33dd5ff10b11fdd703f251b3b9f7b8ed8f79e4
Edmund von der Burg <edmund.vonderburg@gmail.com>
2025-05-21 18:21:38 +0200

    Basic homing of the glint as well

--- GooglyEyeCode/GooglyEyeCode.ino
+++ GooglyEyeCode/GooglyEyeCode.ino
@@ -17,6 +17,8 @@
 #define glintStepperPin2 9
 #define glintStepperPin3 10
 #define glintStepperPin4 11
+#define glintHallEffectPin 12
+
 
 AccelStepper pupilStepper(
   AccelStepper::HALF4WIRE,
@@ -38,39 +40,48 @@ void setup() {
   pupilStepper.setAcceleration(1000.0);
   pupilStepper.moveTo(20480);
 
-  glintStepper.setMaxSpeed(1100);
+  glintStepper.setMaxSpeed(1000);
   glintStepper.setAcceleration(1000.0);
   glintStepper.moveTo(20480);
 
   pinMode(pupilLightGatePin, INPUT_PULLUP);
+  pinMode(glintHallEffectPin, INPUT_PULLUP);
   pinMode(13, OUTPUT);
+
+  Serial.begin(115200);
 }
 
 
 void loop() {
-
-  //Change direction when the stepper reaches the target position
-  //if (glintStepper.distanceToGo() == 0)
-  //  glintStepper.moveTo(-glintStepper.currentPosition());
-
-
-
   // Check to see if we are at midday (lightgate triggers)
   int isMidday = !digitalRead(pupilLightGatePin);
-  digitalWrite(13, isMidday);
+
+  int isOnTheHour = !digitalRead(glintHallEffectPin);
+  digitalWrite(13, isOnTheHour);
 
   // When we see midday save the position and go back there
   if (isMidday)
     pupilStepper.moveTo(pupilStepper.currentPosition());
 
-  // When we have reached
+  // when pupil at home position then home the glint
+  if (pupilStepper.distanceToGo() == 0) {
+    if (isOnTheHour) {
+      glintStepper.moveTo(glintStepper.currentPosition());
+    }
+    //Serial.print("curr:");
+    //Serial.print(glintStepper.currentPosition());
+    //Serial.print(",target:");
+    //Serial.println(glintStepper.targetPosition());
+  }
+
+  pupilStepper.run();
+  glintStepper.run();
+
+  // When we have reached the end power down
   if (
     pupilStepper.distanceToGo() == 0
     && glintStepper.distanceToGo() == 0) {
     pupilStepper.disableOutputs();
     glintStepper.disableOutputs();
   }
-
-
-  pupilStepper.run();
 }
