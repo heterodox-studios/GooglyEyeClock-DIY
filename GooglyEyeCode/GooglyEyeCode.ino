9f62bddae34e3e6d926391f912173076cae70d23
Edmund von der Burg <edmund.vonderburg@gmail.com>
2025-05-21 17:30:18 +0200

    Basic homing of the pupil using the light gate

--- GooglyEyeCode/GooglyEyeCode.ino
+++ GooglyEyeCode/GooglyEyeCode.ino
@@ -9,6 +9,8 @@
 #define pupilStepperPin2 4
 #define pupilStepperPin3 5
 #define pupilStepperPin4 6
+#define pupilLightGatePin 7
+
 
 // glint pin definitions
 #define glintStepperPin1 8
@@ -30,6 +32,7 @@ AccelStepper glintStepper(
   glintStepperPin2,
   glintStepperPin4);
 
+
 void setup() {
   pupilStepper.setMaxSpeed(1000);
   pupilStepper.setAcceleration(1000.0);
@@ -37,20 +40,37 @@ void setup() {
 
   glintStepper.setMaxSpeed(1100);
   glintStepper.setAcceleration(1000.0);
-  glintStepper.moveTo(-20480/2);
+  glintStepper.moveTo(20480);
+
+  pinMode(pupilLightGatePin, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
 }
 
 
-  void loop() {
+void loop() {
 
-    //Change direction when the stepper reaches the target position
-    if (pupilStepper.distanceToGo() == 0)
-      pupilStepper.moveTo(-pupilStepper.currentPosition());
-
-    if (glintStepper.distanceToGo() == 0)
-      glintStepper.moveTo(-glintStepper.currentPosition());
+  //Change direction when the stepper reaches the target position
+  //if (glintStepper.distanceToGo() == 0)
+  //  glintStepper.moveTo(-glintStepper.currentPosition());
 
 
-    pupilStepper.run();
-    glintStepper.run();
+
+  // Check to see if we are at midday (lightgate triggers)
+  int isMidday = !digitalRead(pupilLightGatePin);
+  digitalWrite(13, isMidday);
+
+  // When we see midday save the position and go back there
+  if (isMidday)
+    pupilStepper.moveTo(pupilStepper.currentPosition());
+
+  // When we have reached
+  if (
+    pupilStepper.distanceToGo() == 0
+    && glintStepper.distanceToGo() == 0) {
+    pupilStepper.disableOutputs();
+    glintStepper.disableOutputs();
   }
+
+
+  pupilStepper.run();
+}
