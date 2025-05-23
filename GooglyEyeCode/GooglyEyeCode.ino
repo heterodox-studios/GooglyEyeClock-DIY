a9cd241a08b2351f90c980d4de302176740252a9
Edmund von der Burg <edmund.vonderburg@gmail.com>
2025-05-23 09:39:59 +0200

    Move configuration to its own file

--- GooglyEyeCode/GooglyEyeCode.ino
+++ GooglyEyeCode/GooglyEyeCode.ino
@@ -1,23 +1,9 @@
 
 // Test that the pupils can move the components- may need to change the gearing a few times
 
-
+#include "config.h"
 #include <AccelStepper.h>
 
-// pupil pin definitions
-#define pupilStepperPin1 3
-#define pupilStepperPin2 4
-#define pupilStepperPin3 5
-#define pupilStepperPin4 6
-#define pupilLightGatePin 7
-
-
-// glint pin definitions
-#define glintStepperPin1 8
-#define glintStepperPin2 9
-#define glintStepperPin3 10
-#define glintStepperPin4 11
-#define glintHallEffectPin 12
 
 
 AccelStepper pupilStepper(
---
+++ GooglyEyeCode/config.h
@@ -0,0 +1,14 @@
+
+// pupil pin definitions
+#define pupilStepperPin1 3
+#define pupilStepperPin2 4
+#define pupilStepperPin3 5
+#define pupilStepperPin4 6
+#define pupilLightGatePin 7
+
+// glint pin definitions
+#define glintStepperPin1 8
+#define glintStepperPin2 9
+#define glintStepperPin3 10
+#define glintStepperPin4 11
+#define glintHallEffectPin 12
