3433095e4f9b8c7859ad4482cd128995d26bf2e8
Edmund von der Burg <edmund.vonderburg@gmail.com>
2025-05-26 09:55:58 +0200

    Move code into header file

--- GooglyEyeCode/Motor.h
+++ GooglyEyeCode/Motor.h
@@ -19,14 +19,182 @@ public:
     int pin3,
     int pin4,
     int homeSensorPin,
-    bool reverse_direction = false);
-  void calibrate();
-  float measure_angle_to_home();
+    bool reverse_direction = false) {
+    _name = name;
+    _pin1 = pin1;
+    _pin2 = pin2;
+    _pin3 = pin3;
+    _pin4 = pin4;
+    _homeSensorPin = homeSensorPin;
 
-  float goto_angle(float angle);
-  void adjust_angle(float angle);
-  void sleep();
-  String debug(String note = "");
+    /*
+    For the 28BYJ-48 stepper motor, the wiring by default is as follows:
+    - IN1   Blue    B- 
+    - IN2   Pink    A+
+    - IN3   Yellow  B+
+    - IN4   Orange  A-
+    - Vmot  Red
+    
+    So for a normal setup order is B-, B+, A+, A-  == 1, 3, 2, 4
+
+    To reverse use B+, B-, A-, A+ == 3, 1, 4, 2
+  */
+
+    if (reverse_direction) {
+      _stepper = AccelStepper(
+        AccelStepper::HALF4WIRE,
+        _pin3,
+        _pin1,
+        _pin2,
+        _pin4);
+    } else {
+      _stepper = AccelStepper(
+        AccelStepper::HALF4WIRE,
+        _pin1,
+        _pin3,
+        _pin2,
+        _pin4);
+    }
+
+
+    _stepper.setMaxSpeed(1000);
+    _stepper.setAcceleration(1000.0);
+
+    pinMode(_homeSensorPin, INPUT_PULLUP);
+
+    _current_angle = 0;
+
+    // starting values that should be corrected by calibration
+    _steps_per_rotation = 2048 * 2;  // basic value for a 28BYJ stepper
+    _steps_across_home_sensor = 0;
+  };
+  void calibrate() {
+    // rotate detecting when the home position is passed and store the steps per rotation and current angle
+
+    // forget where we think we are and set a target a long way away
+    _stepper.setCurrentPosition(0);
+    _stepper.moveTo(_steps_per_rotation * 100);  // 100 revolutions of motor
+
+    // keep going until we reach home
+    while (_home_detected())
+      _stepper.run();
+    while (!_home_detected())
+      _stepper.run();
+
+    long first_stop = _stepper.currentPosition();
+
+    // go again until we stop again
+    while (_home_detected())
+      _stepper.run();
+    _steps_across_home_sensor = _stepper.currentPosition() - first_stop;
+    while (!_home_detected())
+      _stepper.run();
+
+    long second_stop = _stepper.currentPosition();
+    _steps_per_rotation = second_stop - first_stop;
+
+
+    // stop the motion and return to the center of the home position gently. Remember to overshoot so that we return in the correct direction to avoid the effects of backlash in the gearing.
+    float overshoot_amount = 10.0 / 360 * _steps_per_rotation;
+    float overshoot_pos = second_stop - overshoot_amount;
+
+    _stepper.moveTo(overshoot_pos);
+    while (_stepper.isRunning()) _stepper.run();
+
+    _stepper.moveTo(second_stop + _steps_across_home_sensor / 2);
+    while (_stepper.run()) 1;
+
+    // reset the current angle with the center of home as 0
+    _current_angle = 0;
+  };
+
+  float measure_angle_to_home() {
+    long starting_position = _stepper.currentPosition();
+
+    _stepper.moveTo(starting_position + _steps_per_rotation * 100);  // 100 revolutions of motor
+
+    while (!_home_detected())
+      _stepper.run();
+
+    _stepper.move(_steps_across_home_sensor / 2);
+    while (_stepper.run()) 1;
+
+
+    long found_home = _stepper.currentPosition();
+    long steps_taken = found_home - starting_position;
+    float angle = steps_taken * (360.0 / _steps_per_rotation);
+
+    _current_angle = 0;
+
+    Serial.print("Angle to home: ");
+    Serial.println(angle);
+
+    return angle;
+  };
+
+  float goto_angle(float angle) {
+
+    // if angle is less than current add 360, we only want to move in one direction
+    while (angle < _current_angle) {
+      angle += 360;
+    }
+
+    float angle_delta = angle - _current_angle;
+
+    // calculate the number of steps to move
+    int steps_to_move = round(angle_delta / 360.0 * _steps_per_rotation);
+
+    // move to the target position
+    _stepper.move(steps_to_move);
+    while (_stepper.run()) 1;
+
+    // update the current angle
+    float degrees_per_step = 360.0 / _steps_per_rotation;
+    _current_angle += steps_to_move * degrees_per_step;
+
+    // keep the current angle in the range 0-360
+    while (_current_angle > 360) _current_angle -= 360;
+
+    return angle_delta;
+  };
+
+
+  void adjust_angle(float angle) {
+    _current_angle += angle;
+
+    while (_current_angle >= 360)
+      _current_angle -= 360;
+  };
+
+  void sleep() {
+    _stepper.disableOutputs();
+  };
+
+  String debug(String note = "") {
+    String out = "";
+
+    out += "#### " + _name + " - " + note + " ####\n";
+
+    out += "# _steps_per_rotation: ";
+    out += _steps_per_rotation;
+    out += "\n";
+
+    out += "# _steps_across_home_sensor: ";
+    out += _steps_across_home_sensor;
+    out += "\n";
+
+    out += "# current steps position: ";
+    out += _stepper.currentPosition();
+    out += "\n";
+
+    out += "# current angle: ";
+    out += _current_angle;
+    out += "\n";
+
+    out += "\n";
+
+    return out;
+  };
 private:
   AccelStepper _stepper;
   String _name;
@@ -35,7 +203,9 @@ private:
   int _pin3;
   int _pin4;
   int _homeSensorPin;
-  bool _home_detected();
+  bool _home_detected() {
+    return !digitalRead(_homeSensorPin);
+  };
 
   float _current_angle;
 
