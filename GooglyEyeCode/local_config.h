06ca8ef6c3295a22ccaf2a60af37e078f7042dc4
Edmund von der Burg <edmund.vonderburg@gmail.com>
2025-06-17 16:03:40 +0200

    GPIO numbers, not pin numbers...

--- GooglyEyeCode/local_config.h
+++ GooglyEyeCode/local_config.h
@@ -1,14 +1,14 @@
 
 // pupil pin definitions
-#define pupilStepperPin1 14
-#define pupilStepperPin2 15
-#define pupilStepperPin3 16
-#define pupilStepperPin4 17
-#define pupilLightGatePin 19
+#define pupilStepperPin1 10
+#define pupilStepperPin2 11
+#define pupilStepperPin3 12
+#define pupilStepperPin4 13
+#define pupilLightGatePin 14
 
 // glint pin definitions
 #define glintStepperPin1 6
 #define glintStepperPin2 7
 #define glintStepperPin3 8
 #define glintStepperPin4 9
-#define glintHallEffectPin 20
+#define glintHallEffectPin 15
