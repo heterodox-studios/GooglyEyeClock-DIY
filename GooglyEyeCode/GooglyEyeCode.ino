5a5da74413f9a66147d68a9f22b6b3bb1434c488
Edmund von der Burg <edmund.vonderburg@gmail.com>
2025-06-21 11:44:59 +0200

    Initial ISR code - commented out

--- GooglyEyeCode/GooglyEyeCode.ino
+++ GooglyEyeCode/GooglyEyeCode.ino
@@ -15,6 +15,8 @@ void setup()
     if (!Serial)
       delay(200); // wait for serial port to connect. Needed for native USB
 
+  // setup_interupts();
+
   delay(5000); // wait for serial monitor to open
 
   Serial.println("------------------------------------");
@@ -25,8 +27,49 @@ void setup()
   timekeeper.setup();
 }
 
+// void setup_interupts()
+// {
+// // attach interrupt to pupil sensor
+// pinMode(pupilLightGatePin, INPUT_PULLUP);
+// attachInterrupt(
+//     digitalPinToInterrupt(pupilLightGatePin),
+//     lightGateISR,
+//     CHANGE);
+
+//   pinMode(glintHallEffectPin, INPUT_PULLUP);
+//   attachInterrupt(
+//       digitalPinToInterrupt(glintHallEffectPin),
+//       glintHallEffectISR,
+//       CHANGE);
+// }
+
+// volatile bool pupilLightGateLastSeenState = LOW;
+// void lightGateISR()
+// {
+//   bool state = digitalRead(pupilLightGatePin);
+//   if (state != pupilLightGateLastSeenState)
+//   {
+//     pupilLightGateLastSeenState = state;
+//     // Serial.println("pupilLightGateISR triggered: " + String(state));
+//   }
+// }
+
+// volatile bool glintHallEffectLastSeenState = LOW;
+// void glintHallEffectISR()
+// {
+//   bool state = digitalRead(glintHallEffectPin);
+//   if (state != glintHallEffectLastSeenState)
+//   {
+//     glintHallEffectLastSeenState = state;
+//     // Serial.println("glintHallEffectISR triggered: " + String(state));
+//   }
+// }
+
 void loop()
 {
+
+  // swing_past_possible_hall_sensor_trigger_times();
+  // monitor_interrupts();
 
   // for (int hours = 0; hours <= 11; hours++) {
   //   for (int minutes = 0; minutes <= 59; minutes++) {
@@ -37,7 +80,7 @@ void loop()
 
   // update time every minute
   display.displayTime(timekeeper);
-  delay(1000 * (61 - timekeeper.secs()));
+  delay(1000 * (60 - timekeeper.secs()));
 
   // update time continuously
   // Serial.println();
@@ -45,3 +88,72 @@ void loop()
 
   // delay(10 * 1000);
 }
+
+// void swing_past_possible_hall_sensor_trigger_times()
+// {
+//   // display.displayTime(0, 0);
+//   // delay(5000);
+
+//   // go to start time to avoid false reporting of the interupts
+//   // display.displayTime(10, 40);
+
+//   for (int hour = 11; hour <= 11; hour++)
+//   {
+//     for (int minute = 52; minute <= 68; minute++)
+//     {
+//       int m = minute % 60;
+//       int h = m == minute ? hour : hour + 1;
+
+//       bool pupil_before = pupilLightGateLastSeenState;
+//       bool glint_before = glintHallEffectLastSeenState;
+
+//       display.displayTime(h, m);
+
+//       if (pupil_before != pupilLightGateLastSeenState)
+//       {
+//         Serial.print("Pupil light gate changed to " + String(pupilLightGateLastSeenState) + " at ");
+//         Serial.print(h);
+//         Serial.print(":");
+//         Serial.println(m);
+//       }
+//       if (glint_before != glintHallEffectLastSeenState)
+//       {
+//         Serial.print("Glint hall effect changed to " + String(glintHallEffectLastSeenState) + " at ");
+//         Serial.print(h);
+//         Serial.print(":");
+//         Serial.println(m);
+//       }
+//     }
+//     Serial.println("");
+//   }
+
+//   // display.displayTime(0, 0);
+//   // delay(5000);
+// }
+
+// void monitor_interrupts()
+// {
+//   display.displayTime(6, 0);
+
+//   Serial.println("");
+
+//   Serial.println("Monitoring interrupts...");
+
+//   bool pupil_before = pupilLightGateLastSeenState;
+//   bool glint_before = glintHallEffectLastSeenState;
+
+//   while (1)
+//   {
+//     if (pupil_before != pupilLightGateLastSeenState)
+//     {
+//       Serial.println("Pupil light gate changed to " + String(pupilLightGateLastSeenState));
+//       pupil_before = pupilLightGateLastSeenState;
+//     }
+
+//     if (glint_before != glintHallEffectLastSeenState)
+//     {
+//       Serial.println("Glint hall effect changed to " + String(glintHallEffectLastSeenState));
+//       glint_before = glintHallEffectLastSeenState;
+//     }
+//   }
+// }
