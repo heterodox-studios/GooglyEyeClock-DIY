232db54d50220181b1a8b2b6262d55bf508159ef
Edmund von der Burg <edmund.vonderburg@gmail.com>
2025-08-20 10:26:49 +0200

    pre branch checkin

--- GooglyEyeMicropython/hand.py
+++ GooglyEyeMicropython/hand.py
@@ -9,9 +9,6 @@ from motor import PupilMotor, GlintMotor
 
 class Hand:
 
-    #     # two values that are never reset. This may be a bad idea, can cross that bridge when we come to it...
-    #     lifetime_angle = 0
-
     _minimum_angle_delta = 360 / 12 / 60 / 2  # 30 seconds on hour hand
 
     _target_angle = 0
@@ -44,6 +41,7 @@ class Hand:
 
     _need_to_update_after_entry_isr = False
     _enter_position = 0
+    _parent_enter_position = None
     _previous_enter_position = 0
 
     _need_to_update_after_exit_isr = False
@@ -53,15 +51,19 @@ class Hand:
     _steps_per_rotation = 0
     _steps_across_home = 0
     _last_confirmed_noon_position = 0
+    _parent_position_at_noon = None
+    _parent_angle_at_noon = None
+    _parent_drift_factor = 1
     _position_has_been_determined = False
 
-    def __init__(self, name, motor, sensor_pin, sensor_rising_is_enter):
+    def __init__(self, name, motor, sensor_pin, sensor_rising_is_enter, parent=None):
 
         # name can be useful when debugging
         self._name = name
 
-        # setup output pins
+        # setup related bits
         self.motor = motor
+        self.parent = parent
 
         # setup input pins and attach interupt
         self._sensor_rising_is_enter = sensor_rising_is_enter
@@ -70,14 +72,33 @@ class Hand:
 
     @property
     def current_angle(self):
-        noon = self._last_confirmed_noon_position
+        angle = self.full_angle_at_position(
+            self.motor.position, self._last_confirmed_noon_position
+        )
+        return angle % 360
+
+    def full_angle_at_position(self, position, noon_position):
+        own_component = 0
+        parent_component = 0
+
+        # get the angle component resulting from our own rotation
         steps_per_rot = self._steps_per_rotation
         try:
-            delta_to_noon = self.motor.position - noon
-            delta_as_angle = delta_to_noon / steps_per_rot * 360
-            return delta_as_angle % 360
+            delta_to_noon = position - noon_position
+            own_component = delta_to_noon / steps_per_rot * 360
         except ZeroDivisionError:
             raise Exception("Cannot calculate current_angle as have not calibrated yet")
+
+        # get the angle component resulting from parents rotation since we recorded our noon position
+        if self.parent:
+            parent_full_angle = self.parent.full_angle_at_position(
+                self.parent.motor.position, self._parent_position_at_noon
+            )
+            parent_component = self._parent_drift_factor * parent_full_angle
+
+        # print(own_component, parent_component, own_component + parent_component)
+
+        return own_component + parent_component
 
     @property
     def degrees_per_step(self):
@@ -102,12 +123,35 @@ class Hand:
         if self.motor.position % 100 == 0:
             gc.collect()
 
+    def run_to(self, target):
+
+        self.wake()
+        self.target_angle = target
+
+        print("run_to", target, "from", self.current_angle)
+
+        while self.is_moving:
+            self.move_to_target()
+
+        self.sleep()
+
+    def wake(self):
+        self.motor.wake()
+        self.update_after_isr()
+
+    def sleep(self):
+        self.motor.sleep()
+
     ##############
 
     def reset_calibration(self):
         # reset values
+
+        # !! remember to reset the child as well, relies on these values
+
         self._need_to_update_after_entry_isr = False
         self._enter_position = 0
+        self._parent_enter_position = None
         self._previous_enter_position = 0
 
         self._need_to_update_after_exit_isr = False
@@ -118,23 +162,37 @@ class Hand:
         self._steps_across_home = 0
 
         self._last_confirmed_noon_position = 0
+        self._last_confirmed_noon_position_parent = 0
+        self._parent_position_at_noon = None
+        self._parent_angle_at_noon = None
+        self._parent_drift_factor = 1
         self._position_has_been_determined = False
+        self._drfit_has_been_determined = False
 
         self.motor.position = 0
 
-    def fake_calibration(self, steps_per_rotation, steps_across_home):
+    def fast_calibration(
+        self, steps_per_rotation, steps_across_home, parent_drift_factor=1.0
+    ):
         self.reset_calibration()
         self.move_until_calibrated(-1000)
         self.reset_calibration()
         self._steps_per_rotation = steps_per_rotation
         self._steps_across_home = steps_across_home
+        self._parent_drift_factor = parent_drift_factor
+
         self.move_until_calibrated()
 
     def calibrate(self):
         """Spin the motor until we have calibrated, and then stop at noon."""
 
         self.reset_calibration()
+
+        if self.parent:
+            self.parent.run_to(0)
+
         self.move_until_calibrated()
+        self.calibrate_parent_drift()
 
     def move_until_calibrated(self, target=1_000_000):
         # Keep stepping until we are calibrated
@@ -142,13 +200,27 @@ class Hand:
         while not self.has_been_calibrated and self.motor.is_moving:
             self.motor.step()
             self.update_after_isr()
-            gc.collect()
+            if self.motor.position % 100 == 0:
+                gc.collect()
 
         # If we are calibrated move to noon
         if self.has_been_calibrated:
-            self._target_angle = 0
-            while self.is_moving:
-                self.move_to_target()
+            self.run_to(0)
+
+    def calibrate_parent_drift(self):
+        if not self.parent:
+            return
+
+        # Get the parent to do a whole revoultion
+        self.parent.run_to(10)
+        self.parent.run_to(0)
+
+        self.motor.set_target(1000000)
+        while not self.drift_has_been_calibrated and self.motor.is_moving:
+            self.motor.step()
+            self.update_after_isr()
+            if self.motor.position % 100 == 0:
+                gc.collect()
 
     @property
     def has_been_calibrated(self):
@@ -158,106 +230,53 @@ class Hand:
             and self._position_has_been_determined
         )
 
-    #############
+    @property
+    def drift_has_been_calibrated(self):
+        return self._drfit_has_been_determined
 
-    #     def forget_position(self):
-    #         self._position_has_been_determined = False
+    #############################
 
-    # def seek_noon(self):
-    #     """Rotate until we find noon"""
-
-    # if self._position_has_been_determined:
-    #     self.step_to_target_angle(0)
-    # elif self.has_been_calibrated:
-    #     while not self._position_has_been_determined:
-    #         self.step()
-    #         time.sleep_us(self._minimum_interval_between_steps_us)
-    #     self.step_to_target_angle(0)
-    # else:
-    #     self.calibrate()
-
-    #     def step_to_target_angle(self, target):
-
-    #         while True:
-    #             cw_delta = (target - self.current_angle) % 360
-
-    #             if cw_delta < self._minimum_angle_delta:
-    #                 break
-
-    #             self.step()
-    #             time.sleep_us(self._minimum_interval_between_steps_us)
-
-    #     def take_steps(self, num):
-    #         dir = 1 if num > 0 else -1
-    #         while num != 0:
-    #             self.step(dir)
-    #             time.sleep_us(self._minimum_interval_between_steps_us)
-
-    #             num -= dir
-
-    #     def step(self, dir=1):
-
-    #         # prep step (reset if needed) and take it
-    #         self._step_index += dir
-    #         self._step_index = self._step_index % self._step_count
-    #         self.writeStepToPins()
-
-    #         # update our tracking of where we are
-    #         self.position += dir
-    #         self.lifetime_angle += self.degrees_per_step
-
-    #         self.update_after_isr()
-
-    #     def sleep(self):
-    #         self.writeToPins(0, 0, 0, 0)
-
-    #     def wake(self):
-    #         self.writeStepToPins()
-
-    #     def writeStepToPins(self):
-    #         self.writeToPins(
-    #             self._step_sequence[self._step_index][0],
-    #             self._step_sequence[self._step_index][1],
-    #             self._step_sequence[self._step_index][2],
-    #             self._step_sequence[self._step_index][3],
-    #         )
-
-    #     def writeToPins(self, p1, p2, p3, p4):
-    #         self._pin1.value(p1)
-    #         self._pin2.value(p2)
-    #         self._pin3.value(p3)
-    #         self._pin4.value(p4)
-
-    #     #############################
-
-    #     def is_in_home_zone(self):
-    #         return self._sensor_pin.value() == self._sensor_rising_is_enter
+    def is_in_home_zone(self):
+        return self._sensor_pin.value() == self._sensor_rising_is_enter
 
     def isr_cb(self, pin):
 
         # print("isr", self._name, pin.value(), self.motor.position)
 
+        # Magnet may trigger switch at odd times - so make sure pupil is in home zone
+        if self.parent and not self.parent.is_in_home_zone():
+            print("ignoring ", self._name, " isr as parent is not in home zone")
+            return
+
         if pin.value() == self._sensor_rising_is_enter:
             self._need_to_update_after_entry_isr = True
             self._enter_position = self.motor.position
+            if self.parent:
+                self._parent_enter_position = self.parent.motor.position
         else:
             self._need_to_update_after_exit_isr = True
             self._exit_position = self.motor.position
 
     def update_after_isr(self):
         if self._need_to_update_after_entry_isr:
-            self.update_after_isr_entry_to_entry()
+            self.update_after_isr_entry()
             self._need_to_update_after_entry_isr = False
 
         if self._need_to_update_after_exit_isr:
-            self.update_after_isr_entry_to_exit()
+            self.update_after_isr_exit()
             self._need_to_update_after_exit_isr = False
 
-    def update_after_isr_entry_to_entry(self):
+    def update_after_isr_entry(self):
 
         # Get the change in steps, reset last entry point to here
         previous_enter_pos = self._previous_enter_position
         self._previous_enter_position = self._enter_position
+
+        # If we have a parent note when we enter
+        previous_parent_position_at_noon = None
+        if self.parent:
+            previous_parent_position_at_noon = self._parent_position_at_noon
+            self._parent_position_at_noon = self._parent_enter_position
 
         # what is the delta? return if too small (means jitter, or running backwards)
         entry_delta = self._enter_position - previous_enter_pos
@@ -267,8 +286,30 @@ class Hand:
 
         # entry to entry (full rotation)
         if previous_enter_pos:
-            steps_per_rotation = self._enter_position - previous_enter_pos
-            self._steps_per_rotation = steps_per_rotation
+
+            print(
+                self.parent,
+                previous_parent_position_at_noon,
+                self._parent_enter_position,
+            )
+
+            # If we have a parent and parent has moved since last visit to enter,
+            # we can calculate the drift.
+            if (
+                self.parent
+                and previous_parent_position_at_noon != self._parent_enter_position
+            ):
+                parent_angle_delta = self.parent.full_angle_at_position(
+                    self._parent_enter_position, previous_parent_position_at_noon
+                )
+                our_angle_delta = self.full_angle_at_position(
+                    self._enter_position, previous_enter_pos
+                )
+
+                print(parent_angle_delta, our_angle_delta)
+            else:
+                steps_per_rotation = self._enter_position - previous_enter_pos
+                self._steps_per_rotation = steps_per_rotation
 
         print(
             "{2} enter: {0} ({1})".format(
@@ -286,9 +327,13 @@ class Hand:
             steps_to_home_center = self._steps_across_home / 2
             new_noon_position = int(self._enter_position + steps_to_home_center)
 
-            angle_before_noon_update = self.current_angle  # remove me
+            # capture for logging below
             angle_previously_determined = self._position_has_been_determined
+            angle_before_noon_update = (
+                self.current_angle if angle_previously_determined else "???"
+            )
 
+            # store what we need to calculate angle
             self._last_confirmed_noon_position = new_noon_position
             self._position_has_been_determined = True
 
@@ -305,7 +350,7 @@ class Hand:
                     )
                 )
 
-    def update_after_isr_entry_to_exit(self):
+    def update_after_isr_exit(self):
         # entry_to_exit
 
         # Get the change in steps, reset last entry point to here
@@ -340,7 +385,17 @@ class PupilHand(Hand):
         )
 
 
-# class GlintMotor(Motor):
+class GlintHand(Hand):
+
+    def __init__(self, parent):
+        super().__init__(
+            "glint",
+            GlintMotor(),
+            config.glint_stepper["sensor_pin"],
+            config.glint_stepper["sensor_rising_is_enter"],
+            parent=parent,
+        )
+
 
 #     # need to track these as part of calibration as they affect us
 #     _pupil_enter_position = 0
--- GooglyEyeMicropython/main.py
+++ GooglyEyeMicropython/main.py
@@ -1,31 +1,61 @@
 import time, gc
-from hand import PupilHand
+from hand import PupilHand, GlintHand
 
 
 pupil_hand = PupilHand()
+glint_hand = GlintHand(parent=pupil_hand)
 
-pupil_hand.fake_calibration(steps_per_rotation=14_260, steps_across_home=590)
+pupil_hand.fast_calibration(steps_per_rotation=14_260, steps_across_home=590)
 
+glint_hand.calibrate()
 
-def goto(angle):
+# glint_hand.fast_calibration(
+#     steps_per_rotation=14_780, steps_across_home=370, parent_drift_factor=1.4
+# )
+
+anch cha
+def goto(hand, angle):
     time.sleep(1)
-    print("goto", angle, "from", pupil_hand.current_angle)
-    pupil_hand.target_angle = angle
+    print("goto", angle, "from", hand.current_angle)
+    hand.target_angle = angle
 
-    pupil_hand.motor.wake()
+    hand.wake()
 
-    while pupil_hand.is_moving:
-        pupil_hand.move_to_target()
+    while hand.is_moving:
+        hand.move_to_target()
 
-    pupil_hand.motor.sleep()
+    hand.sleep()
 
 
-goto(15)
-goto(15.1)
-goto(14.9)
+# goto(glint_hand, 180)
+# goto(glint_hand, 90)
 
-goto(3 * 360 / 12 + 0 * 360 / 12 / 60)  # 03:00
-goto(3 * 360 / 12 + 1 * 360 / 12 / 60)  # 03:01
 
-for _ in [x / 10.0 for x in range(3520, 3540, 1)]:
-    goto(_)
+# for _ in (glint_hand, pupil_hand):
+#     print(_._name, _.current_angle)
+
+# goto(pupil_hand, 90)
+# for _ in (glint_hand, pupil_hand):
+#     print(_._name, _.current_angle)
+
+# goto(glint_hand, 180)
+# for _ in (glint_hand, pupil_hand):
+#     print(_._name, _.current_angle)
+
+# # set up for next test run
+# time.sleep(10)
+# goto(pupil_hand, 350)
+# goto(glint_hand, 350)
+# for _ in (glint_hand, pupil_hand):
+#     print(_._name, _.current_angle)
+
+
+# goto(pupil_hand, 15)
+# goto(pupil_hand, 15.1)
+# goto(pupil_hand, 14.9)
+
+# goto(pupil_hand, 3 * 360 / 12 + 0 * 360 / 12 / 60)  # 03:00
+# goto(pupil_hand, 3 * 360 / 12 + 1 * 360 / 12 / 60)  # 03:01
+
+# for _ in [x / 10.0 for x in range(3520, 3540, 1)]:
+#     goto(pupil_hand, _)
